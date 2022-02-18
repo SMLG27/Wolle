d76a407fd899a5ad12b3cf03cca88db74d1a5b1b
Angelll <achosss212121@gmail.com>
2022-02-18 13:33:13 +0100

    Project

--- read_excel.py
+++ read_excel.py
@@ -4,7 +4,7 @@ import pandas as pd
 class ExcelData:
 
     def __init__(self):
-        data = pd.read_excel('C:\\Users\\achos\\OneDrive\\Desktop\\Produkt_list.xlsx', index_col=[0,1])
+        data = pd.read_excel('C:\\Users\\achos\\PycharmProjects\\Wolle\\Produkt_list.xlsx', index_col=[0,1])
         self.exc_data = list(data.index)
         self.produkt_list = None
         self.produkt_only_name = None
@@ -13,9 +13,3 @@ class ExcelData:
         self.produkt_list = ["-".join(" ".join(e).split()) for e in self.exc_data]
         self.produkt_only_name = [(" ".join(e).split())[0] for e in self.exc_data]
         return self.produkt_list
-
-
-if __name__ == '__main__':
-    excel = ExcelData()
-    excel.produkt_l()
-
